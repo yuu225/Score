82cf4780583dbe7572aadb0322d55cf7f8bb20ec
yuu225 <2470637775@qq.com>
2025-05-29 11:58:38 +0800

    更新网站：修复基础路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath } from 'url'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Score/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   server: {
     port: 5173,
